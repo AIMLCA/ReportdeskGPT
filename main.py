ebc6a93fa3c1dbaba17556dd12e8e0b5f8ccb9df
AIMLCloud <128804019+AIMLCloud@users.noreply.github.com>
2023-05-25 18:51:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,60 @@ import openai
 import mysql.connector
 from flask_cors import CORS
 import json
+import sqlite3
+import pinecone
+from langchain.vectorstores import Pinecone
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 CORS(app)
 
+app.secret_key = 'secret_key'
+
+# Database configuration
+DATABASE = 'database.db'
+
+def create_table():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+    # Create a users table if it doesn't exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            phone TEXT UNIQUE NOT NULL,
+            organisation TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+
+
+    conn.commit()
+    conn.close()
+
+    # Create Question Table
+    conn2 = sqlite3.connect(DATABASE)
+    cursor2 = conn2.cursor()
+
+    # Create a users table if it doesn't exist
+    cursor2.execute('''
+        CREATE TABLE IF NOT EXISTS questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT UNIQUE NOT NULL,
+            answer TEXT UNIQUE NOT NULL,
+            useremail TEXT UNIQUE NOT NULL
+            
+        )
+    ''')
+
+    conn2.commit()
+    conn2.close()
+
+# Create the users table
+create_table()
 
 
 @app.route("/")
@@ -40,7 +89,7 @@ def answer():
     print(email)
     # data = json.loads(json_string)
     print(ques)
-    healthcare_words = ["medications","drugs", "pills", "tablets", "prescriptions", "dosages","diabetes", "cancer", "heart disease","covid-19" "asthma", "arthritis", "Alzheimer's disease", "Parkinson's disease", "mental health conditions", "infectious diseases","doctors", "nurses", "pharmacists", "surgeons", 'therapists', 'chiropractors', "acupuncturists", 'midwives',"hospitals", "clinics", "urgent care centers", "rehabilitation centers", "nursing homes", "hospices", "home health agencies", "policies", "premiums", "deductibles", "copays", "coverage", "claims", "network", "providers","fitness", "nutrition", "weight loss", "smoking cessation", "stress management", "sleep", "mental health", "addiction treatment","blood pressure monitors", "glucose meters", "prosthetics", "hearing aids", "wheelchairs", "crutches", "braces", "inhalers","surgeries", "diagnostic tests", "imaging studies", "biopsies", "vaccinations", "screenings", "check-ups","disease prevention", "vaccination campaigns", "epidemiology", "health promotion", "health policy", "disaster preparedness","electronic health records", "telemedicine", "health apps", "wearable devices", "AI in healthcare","double helix", "nucleotide", "gene", "chromosome", "base pair", "genome sequence", "mutation", "genetic code", "genetic variation","gene expression", "genetic mapping", "epigenetics", "transcriptomics", "proteomics", "metabolomics","cystic fibrosis", "sickle cell anemia", "Huntington's disease", "muscular dystrophy", "Down syndrome", "hemophilia", "thalassemia","CRISPR", "gene therapy", "genome engineering", "targeted gene editing","pharmacogenomics", "genetic testing", "genetic counseling", "precision medicine", "gene-based diagnosis", "gene-based therapy","population genetics", "evolutionary genetics", "molecular genetics", "comparative genomics", "functional genomics","genetic engineering", "synthetic biology", "gene synthesis", "gene cloning", "genetic modification", "recombinant DNA technology","genetic discrimination", "privacy concerns", "informed consent", "gene patenting", "intellectual property rights", "genetically modified organisms","whole genome sequencing", "targeted sequencing", "high-throughput sequencing", "next-generation sequencing", "nanopore sequencing","agriculture and livestock breeding", "forensic science", "paleogenomics", "conservation biology", "ancestry testing", "evolutionary biology", "Tay-Sachs disease", "bioinformatics", "side effects", "interactions" "diseases", "providers", "facilities", "insurance", "wellness", "equipment", "procedures", "public health", "technology"]
+    healthcare_words = ["medications","drugs", "pills", "tablets", "prescriptions", "dosages","diabetes", "cancer", "heart disease","covid-19" "asthma","genomics", "arthritis", "Alzheimer's disease", "Parkinson's disease", "mental health conditions", "infectious diseases","doctors", "nurses", "pharmacists", "surgeons", 'therapists', 'chiropractors', "acupuncturists", 'midwives',"hospitals", "clinics", "urgent care centers", "rehabilitation centers", "nursing homes", "hospices", "home health agencies", "policies", "premiums", "deductibles", "copays", "coverage", "claims", "network", "providers","fitness", "nutrition", "weight loss", "smoking cessation", "stress management", "sleep", "mental health", "addiction treatment","blood pressure monitors", "glucose meters", "prosthetics", "hearing aids", "wheelchairs", "crutches", "braces", "inhalers","surgeries", "diagnostic tests", "imaging studies", "biopsies", "vaccinations", "screenings", "check-ups","disease prevention", "vaccination campaigns", "epidemiology", "health promotion", "health policy", "disaster preparedness","electronic health records", "telemedicine", "health apps", "wearable devices", "AI in healthcare","double helix", "nucleotide", "gene", "chromosome", "base pair", "genome sequence", "mutation", "genetic code", "genetic variation","gene expression", "genetic mapping", "epigenetics", "transcriptomics", "proteomics", "metabolomics","cystic fibrosis", "sickle cell anemia", "Huntington's disease", "muscular dystrophy", "Down syndrome", "hemophilia", "thalassemia","CRISPR", "gene therapy", "genome engineering", "targeted gene editing","pharmacogenomics", "genetic testing", "genetic counseling", "precision medicine", "gene-based diagnosis", "gene-based therapy","population genetics", "evolutionary genetics", "molecular genetics", "comparative genomics", "functional genomics","genetic engineering", "synthetic biology", "gene synthesis", "gene cloning", "genetic modification", "recombinant DNA technology","genetic discrimination", "privacy concerns", "informed consent", "gene patenting", "intellectual property rights", "genetically modified organisms","whole genome sequencing", "targeted sequencing", "high-throughput sequencing", "next-generation sequencing", "nanopore sequencing","agriculture and livestock breeding", "forensic science", "paleogenomics", "conservation biology", "ancestry testing", "evolutionary biology", "Tay-Sachs disease", "bioinformatics", "side effects", "interactions" "diseases", "providers", "facilities", "insurance", "wellness", "equipment", "procedures", "public health", "technology"]
 
     def is_healthcare_question(question):
         for word in healthcare_words:
@@ -52,10 +101,10 @@ def answer():
     user_input = ques
     if is_healthcare_question(user_input):
             # Process the question with the chatbot
-        connection = create_connection()
+        connection = sqlite3.connect(DATABASE)
         cursor = connection.cursor()
         
-        cursor.execute("SELECT answer FROM questions where question = '" + ques +"'")
+        cursor.execute("SELECT answer FROM questions where question =?", (ques,))
         result = cursor.fetchall()
         print("Result: ", result) 
         
@@ -70,41 +119,42 @@ def answer():
             # return response
             return result[0][0]
             
-        else:
-            ## If no match is found, ask chatGPT and return the answers1 = 'Hello'
-            s2 = ''
-            s1 = ques
-            print(ques)
-            s3 = "{} {}".format(s2, s1)
-            response = openai.Completion.create(
-                engine = "text-davinci-003",
-                prompt = s3,
+        else :
+                
+                ## If no match is found, ask chatGPT and return the answers1 = 'Hello'
+                s2 = ''
+                s1 = ques
+                print(ques)
+                s3 = "{} {}".format(s2, s1)
+                response = openai.Completion.create(
+                    engine = "text-davinci-003",
+                    prompt = s3,
+                
+                    max_tokens = 1500,
+                    n = 1,
+                    stop = None,
+                    temperature=0.5,
+                    timeout = 15,
+                )
             
-                max_tokens = 1500,
-                n = 1,
-                stop = None,
-                temperature=0.5,
-                timeout = 15,
-            )
+                answer = response.choices[0].text.strip()
+                
+                ## Store the new question and their answer in the database
+                
+                
+                cursor.execute("INSERT INTO questions (question, answer,useremail) VALUES (?,?,?)", (ques, answer, email))
+                ## Returning new answer as response
+                connection.commit()
         
-            answer = response.choices[0].text.strip()
-            
-            ## Store the new question and their answer in the database
-            
-            
-            cursor.execute("INSERT INTO questions (question, answer,useremail) VALUES (%s, %s, %s)", (ques, answer, email))
-            ## Returning new answer as response
-            connection.commit()
-    
-            #Closing the cursor
-            cursor.close()
-            print(answer)
-            # response = Response(answer)
-            # response.headers.add('Access-Control-Allow-Origin', '*')
-            # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-            # response.headers.add('Access-Control-Allow-Methods', 'POST')
-            # return response
-            return answer
+                #Closing the cursor
+                cursor.close()
+                print(answer)
+                # response = Response(answer)
+                # response.headers.add('Access-Control-Allow-Origin', '*')
+                # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+                # response.headers.add('Access-Control-Allow-Methods', 'POST')
+                # return response
+                return answer
 
     else:
         return "Sorry, only healthcare-related questions are allowed."
@@ -112,7 +162,7 @@ def answer():
 
 
 ## Configure OpenAI API Key
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 
@@ -125,10 +175,10 @@ def saveCustomer():
     phone = request.form.get('phone')
     Orgnization = request.form.get('Orgnization')
     
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
-    cursor.execute("SELECT * FROM users where email = '" + email +"'")
+    cursor.execute("SELECT * FROM users where email = ?", (email,))
     result = cursor.fetchall()
     print("Result: ", result) 
        
@@ -143,7 +193,7 @@ def saveCustomer():
         ## Store the new question and their answer in the database
         
         
-        cursor.execute("INSERT INTO users (name, password,email,phone,organisation) VALUES (%s, %s,%s, %s, %s)", (name, password,email,phone,Orgnization))
+        cursor.execute("INSERT INTO users (name, password,email,phone,organisation) VALUES (?,?,?,?,?)", (name, password,email,phone,Orgnization))
         ## Returning new answer as response
         connection.commit()
  
@@ -164,10 +214,10 @@ def signIn():
     email = request.form.get('email')
     password = request.form.get('password')
   
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
-    cursor.execute("SELECT * FROM users where email = '" + email +"'")
+    cursor.execute("SELECT * FROM users where email = ?" ,(email,))
     result = cursor.fetchall()
     print("Result: ", result) 
 
@@ -175,7 +225,7 @@ def signIn():
     if result[0][2] ==password:
         ## If a match is found, return the answer 
         print("Result: ", result)
-        cursor.execute("SELECT * FROM questions where useremail = '" + email +"'")
+        cursor.execute("SELECT * FROM questions where useremail = ?", (email,))
         result = cursor.fetchall()
         return jsonify(result)
     else:
@@ -190,10 +240,10 @@ def fetchhistory():
     email = request.form.get('email')
     
   
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
-    cursor.execute("SELECT * FROM questions where useremail = '" + email +"'")
+    cursor.execute("SELECT * FROM questions where useremail = ?", (email,))
     result = cursor.fetchall()
     print("Result: ", result) 
 
@@ -201,29 +251,12 @@ def fetchhistory():
     if result:
         ## If a match is found, return the answer 
         print("Result: ", result)
-        cursor.execute("SELECT * FROM questions where useremail = '" + email +"'")
+        cursor.execute("SELECT * FROM questions where useremail = ?", (email,))
         result = cursor.fetchall()
         return jsonify(result)
     else:
         return "No Search History"
-# Load questions from the Excel file
 
-
-## Configure database connection
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'flask'
-# mysql = MySQL(app)
-
-# cursor = mysql.connection.cursor()
-
-# db_engine = create_engine('postgresql://postgres:imgpt@localhost/imgpt')
-#db_engine = create_engine('db-instance-url-goes-here-with-database-name')
-
-
-
-## Function to match the incoming question 
 
 @app.route('/getAnswer', methods=['POST','GET'])
 def match_question():
@@ -237,10 +270,10 @@ def match_question():
     print("request.data")
     
     
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
-    cursor.execute("SELECT question, answer FROM questions where question = '" + ques +"'")
+    cursor.execute("SELECT question, answer FROM questions where question = ?", (ques,))
     result = cursor.fetchall()
     print("Result: ", result) 
        
@@ -277,7 +310,7 @@ def match_question():
         ## Store the new question and their answer in the database
         
         
-        cursor.execute("INSERT INTO questions (question, answer) VALUES (%s, %s)", (ques, answer))
+        cursor.execute("INSERT INTO questions (question, answer) VALUES (?, ?)", (ques, answer))
         ## Returning new answer as response
         connection.commit()
  
@@ -306,7 +339,7 @@ def train_model():
     connection = create_connection()
     cursor = connection.cursor()
     
-    cursor.execute("SELECT question, answer FROM questions where question = '" + ques +"'")
+    cursor.execute("SELECT question, answer FROM questions where question = ?", (ques,))
     result = cursor.fetchall()
     print("Result: ", result) 
        
@@ -322,7 +355,7 @@ def train_model():
 
         ## Store the new question and their answer in the database
         
-        cursor.execute("INSERT INTO questions (question, answer) VALUES (%s, %s)", (ques, answer))
+        cursor.execute("INSERT INTO questions (question, answer) VALUES (?, ?)", (ques, answer))
         ## Returning new answer as response
         connection.commit()
  
@@ -342,7 +375,7 @@ def train_model():
 
 def alluser():
     
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
     cursor.execute("SELECT * FROM users")
@@ -373,7 +406,7 @@ def alluser():
 
 def allquestion():
     
-    connection = create_connection()
+    connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     
     cursor.execute("SELECT * FROM questions")
@@ -397,4 +430,36 @@ def allquestion():
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         response.headers.add('Access-Control-Allow-Methods', 'POST')
         return response
+    
+
+
+def QueryFromPinecone():
+   
+    pinecone.init(
+        api_key="a292ca62-1b92-4530-a48e-de857a49e33b",
+        environment="us-west1-gcp-free"
+    )
+
+    index_name = "repodex"
+
+    index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+    def get_similiar_docs(query, k=2, score=False):
+        if score:
+            similar_docs = index.similarity_search_with_score(query, k=k)
+        else:
+            similar_docs = index.similarity_search(query, k=k)
+        return similar_docs
+    
+    model_name = "text-davinci-003"
+    # model_name = "gpt-3.5-turbo"
+    #model_name = "gpt-4"
+    llm = openai(model_name=model_name)
+
+    chain = load_qa_chain(llm, chain_type="stuff")
+
+    def get_answer(query):
+        similar_docs = get_similiar_docs(query)
+        answer = chain.run(input_documents=similar_docs, question=query)
+        return answer
+
 app.run(debug=True)
